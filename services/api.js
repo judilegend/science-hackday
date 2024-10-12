1ec50aacb6a597fb0dcecd87075da94c3ea1483b
judilegend <judilegend2@gmail.com>
2024-10-12 15:54:19 +0300

    feat: demander une urgence

--- services/api.js
+++ services/api.js
@@ -23,9 +23,35 @@ export const authenticateUser = async (username, password) => {
 
 export const registerUser = async (userData) => {
   try {
-    const response = await api.post("/user/authenticate", userData);
+    const response = await api.post("/user/create", userData);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Registration failed");
+  }
+};
+export const reportEmergency = async (latitude, longitude) => {
+  try {
+    const response = await api.post("/emergency/report", {
+      latitude,
+      longitude,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || "Failed to report emergency"
+    );
+  }
+};
+export const reportIssue = async (title, description, latitude, longitude) => {
+  try {
+    const response = await api.post("/issues/report", {
+      title,
+      description,
+      latitude,
+      longitude,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message || "Failed to report issue");
   }
 };
